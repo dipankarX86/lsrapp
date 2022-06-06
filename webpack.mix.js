21c6639679c7f509ab0f1e7c9b62df055f624982
Dipankar Saikia <er.dipankarsaikia@gmail.com>
2022-06-06 20:32:44 +0530

    the laravel mix requirements are removed from the project, (LADUMOR removed for now)

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,26 +18,26 @@ mix.js('resources/js/app.js', 'public/js')
 
 
 // following part is for react
-mix.options({
-    postCss: [
-        require('autoprefixer'),
-    ],
-});
+// mix.options({
+//     postCss: [
+//         require('autoprefixer'),
+//     ],
+// });
 
-mix.setPublicPath('public');
+// mix.setPublicPath('public');
 
-mix.webpackConfig({
-    resolve: {
-        extensions: ['.js', '.vue'],
-        alias: {
-            '@': __dirname + 'resources'
-        }
-    },
-    output: {
-        chunkFilename: 'js/chunks/[name].js',
-    },
-}).react();
+// mix.webpackConfig({
+//     resolve: {
+//         extensions: ['.js', '.vue'],
+//         alias: {
+//             '@': __dirname + 'resources'
+//         }
+//     },
+//     output: {
+//         chunkFilename: 'js/chunks/[name].js',
+//     },
+// }).react();
 
-// used to run app using reactjs
-mix.js('resources/react-app/src/index.js', 'public/js/app.js').version();
-mix.copy('resources/react-app/src/assets', 'public/assets');
+// // used to run app using reactjs
+// mix.js('resources/react-app/src/index.js', 'public/js/app.js').version();
+// mix.copy('resources/react-app/src/assets', 'public/assets');
